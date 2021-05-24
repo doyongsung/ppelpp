30d9eb1021981fe55b596398bc0d0f7b886de1cd
doyongsung <ppelpp@naver.com>
2021-05-24 16:11:00 +0900

    Update Calculator.java

--- JavaProject/FirstJava/src/test/Calculator.java
+++ JavaProject/FirstJava/src/test/Calculator.java
@@ -1,21 +1,40 @@
 package test;
 
 public class Calculator {
-	int add(int a, int b) { return a + b;}
-
-    int sub (int a, int b) {return a - b;}
-    int mul (int a, int b) {return a * b;}
-    int iMul (int a, int b) {return a / b;}
-    
-	public static void main(String[] args) {
-		
-		Calculator cal = new Calculator();
-		
-		System.out.println(cal.add(5, 5));
-		System.out.println(cal.sub(5, 5));
-		System.out.println(cal.mul(5, 5));
-		System.out.println(cal.iMul(5, 5));
-
+	
+	//메소드 -> 기능 ( 재료 -> 처리 -> 반환)
+	//반환타입 메소드 이름(매개변수들){ 처리 구문}
+	void plus (int n1, int n2) {
+		System.out.println(n1 + n2);
 	}
-
+	
+	void minus(int n1, int n2) {
+		System.out.println(n1 - n2);
+	}
+	
+	void multi(int n1, int n2) {
+		System.out.println(n1 * n2);
+	}
+	
+	void div(int n1, int n2) {
+		System.out.println(n1 / n2);
+	}
 }
+//	int add(int a, int b) { return a + b;}
+//
+//    int sub (int a, int b) {return a - b;}
+//    int mul (int a, int b) {return a * b;}
+//    int iMul (int a, int b) {return a / b;}
+//    
+//	public static void main(String[] args) {
+//		
+//		Calculator cal = new Calculator();
+//		
+//		System.out.println(cal.add(5, 5));
+//		System.out.println(cal.sub(5, 5));
+//		System.out.println(cal.mul(5, 5));
+//		System.out.println(cal.iMul(5, 5));
+//
+//	}
+//
+//}
