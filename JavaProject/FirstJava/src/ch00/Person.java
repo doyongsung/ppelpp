1e27f5b7a40da01437b7a4539bc592da9be4aaa7
doyongsung <ppelpp@naver.com>
2021-05-25 11:14:13 +0900

    ch00

---
+++ JavaProject/FirstJava/src/ch00/Person.java
@@ -0,0 +1,33 @@
+package ch00;
+
+public class Person {
+	//클래수의 구성 -> 인스턴수 변수, 메소드
+	
+	// 회원이름을 저장하는 변수
+	String name = "도용성"; //초기화를 생략가능
+	
+	//회원 전화 번호("000-0000-0000"//를 저장하는 변수
+	String memberPhonNum = "000-0000-0000";
+	
+	//회워 주민번호 등록시 (00000000000 또는000000000000) 를 저장하는변수{
+	String perNum1 = "74000000 - 000000000";
+	long perNum2 = 7000000000L;
+	
+	//인스턴수 변수들의 데이터를 출력하는 메소드.
+	void showData() {
+		System.out.println("회원 이름 : " + this.name);
+		System.out.println("회원의 주민번호1 : " + perNum1);
+		System.out.println("회원의 주민번호2 : " + perNum2);
+	}
+	public static void main(String[] args) {
+		
+		//Person 인스턴스 생성
+		Person person = new Person();
+		
+		System.out.println("이름 : " + person.name);
+		person.name = "손흥민";
+		person.showData();
+	
+					}
+
+}
--- JavaProject/FirstJava/src/test/Member.java
+++ JavaProject/FirstJava/src/test/Member.java
@@ -6,9 +6,30 @@ public class Member {
 
 	public static void main(String[] args) {
 		
+		//변수 선언
+		//데이터의 타입 식별 이름
+		//데이터 타입 -> 내가 다루어야 하는 데이터의 특징에 의해서 결정
+		//기본형 타입 8가지 -> 정수형 ,실수형 ,boolean , 문자
 		
-		System.out.println("도용성");
-
+		String name = "도용성";
+		int age = 3;
+		double height = 130.5d;
+		float weight = 45.8f;
+		boolean hasBook = true;
+		
+		//System.out.println(name +" " +age +" "+ heigth +" "+ weight + " "+ hasBook);
+		
+		System.out.println("저의 이름은 " + name);//String + String
+		System.out.println("키는 " + height + "cm 입니다."); // String + double + String -> (String + String) + String -> String + String
+		//String + any Type -> String + String 
+	    System.out.println("제 나이는 " + age + "살 입니다."); //String + int + String
+	    System.out.println("책의 보유 여부 : " + hasBook); //String + boolean
+	    
+	    if(hasBook) {
+	    	System.out.println("책 있어요!!");
+	    }else {
+	    	System.out.println("책 없어요!!");
+	    }
 	}
 
 }
