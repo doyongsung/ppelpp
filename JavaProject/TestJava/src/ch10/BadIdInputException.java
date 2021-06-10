31cd2ccd877b0358db81784d19233da59a0a1376
doyongsung <ppelpp@naver.com>
2021-06-10 09:45:12 +0900

    20210610

--- JavaProject/TestJava/src/ch10/BadIdInputException.java
+++
@@ -1,9 +0,0 @@
-package ch10;
-
-public class BadIdInputException extends Exception{
-
-	public BadIdInputException(String message) {
-		super(message);
-	}
-
-}
--- JavaProject/TestProject/src/ch01/Person.java
+++
@@ -1,8 +0,0 @@
-package ch01;
-
-public class Person {
-
-	String name = "도용성";
-	String phone = "010-5508-6297";
-	String Number = "940403-1111111";
-	}
