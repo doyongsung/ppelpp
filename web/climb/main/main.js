6f8193815c9d089fc890528e7d120cab4dd2c16c
doyongsung <ppelpp@naver.com>
2021-08-22 20:33:21 +0900

    final project

--- web/climb/main/main.js
+++ web/climb/main/main.js
@@ -9,3 +9,11 @@ $(function(){
         
 	});
 });
+var $header = $('header') ;
+$(window).scroll(function(){
+	if($(this).scrollTop() > 0){
+		$header.addClass('sticky');
+	}else{
+		$header.removeClass('sticky');
+	}
+})
